fbfcb78110edfb22ad4fee305c38ff2b742523b9
Lalit Singh <lalitthesingh@gmail.com>
2017-07-08 01:59:50 +0530

    Errors Corrected

--- app/src/main/java/com/example/admin/successcoachingapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/admin/successcoachingapp/HomeScreenActivity.java
@@ -1,23 +1,93 @@
 package com.example.admin.successcoachingapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeScreenActivity extends AppCompatActivity {
+import java.util.List;
+
+public class HomeScreenActivity extends AppCompatActivity implements View.OnClickListener {
+
+     Toolbar toolbar;
+     TextView titleView,aboutUs,staff,courses;
+     ImageView testimonial,gallery,qr,map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //same layout file as staff screen.
         setContentView(R.layout.activity_home_screen);
+        init();
+        titleView.setText("COURSES");
+        setSupportActionBar(toolbar);
 
-
+        testimonial.setOnClickListener(this);
+        gallery.setOnClickListener(this);
+        qr.setOnClickListener(this);
+        map.setOnClickListener(this);
+        aboutUs.setOnClickListener(this);
+        courses.setOnClickListener(this);
+        staff.setOnClickListener(this);
 
     }
     public void onClick(View view){
-        Intent intent = new Intent(HomeScreenActivity.this,OurStaffActivity.class);
-        startActivity(intent);
+        Intent i;
+        switch (view.getId()){
+            case R.id.ivTestimonial:
+                i=new Intent(HomeScreenActivity.this,TestimonialActivity.class);
+                startActivity(i);
+                break;
+            case R.id.ivGallery:
+                i=new Intent(HomeScreenActivity.this,GalleryActivity.class);
+                startActivity(i);
+                break;
+            case R.id.ivQrCodes:
+                i = new Intent("com.google.zxing.client.android.SCAN");
+                i.setPackage("com.google.zxing.client.android");
+                i.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(i, PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+                if(isIntentSafe) {
+                    startActivityForResult(i, 0);
+                }else {
+                    i = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.zxing.client.android"));
+                    startActivity(i);
+                }
+                break;
+            case R.id.ivMap:
+
+                break;
+            case R.id.tvAboutUs:
+                i =new Intent(HomeScreenActivity.this,AboutUsActivity.class);
+                startActivity(i);
+                break;
+            case R.id.tvCourses:
+                i =new Intent(HomeScreenActivity.this,Courses.class);
+                startActivity(i);
+                break;
+            case R.id.tvStaff:
+                i =new Intent(HomeScreenActivity.this,OurStaffActivity.class);
+                startActivity(i);
+                break;
+        }
+
+    }
+    public void init() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        titleView= (TextView) toolbar.findViewById(R.id.title);
+        testimonial= (ImageView) findViewById(R.id.ivTestimonial);
+        gallery= (ImageView) findViewById(R.id.ivGallery);
+        qr= (ImageView) findViewById(R.id.ivQrCodes);
+        map= (ImageView) findViewById(R.id.ivMap);
+        aboutUs= (TextView) findViewById(R.id.tvAboutUs);
+        courses= (TextView) findViewById(R.id.tvCourses);
+        staff= (TextView) findViewById(R.id.tvStaff);
     }
 }
