b5d36bc11b45ab4934a150c89162bbbe208a7533
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-07-07 22:16:52 +0530

    ui changes in home screen

---
+++ app/src/main/java/com/example/admin/successcoachingapp/ColorUtils.java
@@ -0,0 +1,34 @@
+package com.example.admin.successcoachingapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by admin on 7/1/2017.
+ */
+
+public class ColorUtils {
+
+    public static int getBackgroundColor(Context context, int instanceNum){
+        switch (instanceNum){
+            case 0:
+                    return ContextCompat.getColor(context, R.color.orange);
+            case 1:
+                    return ContextCompat.getColor(context,R.color.green);
+            case 2:
+                    return ContextCompat.getColor(context,R.color.oliveGreen);
+            case 3:
+                    return ContextCompat.getColor(context, R.color.orange);
+            case 4:
+                    return ContextCompat.getColor(context,R.color.green);
+            case 5:
+                    return ContextCompat.getColor(context,R.color.oliveGreen);
+            case 6:
+                    return ContextCompat.getColor(context, R.color.orange);
+            default:
+                    return Color.WHITE;
+
+        }
+    }
+}
