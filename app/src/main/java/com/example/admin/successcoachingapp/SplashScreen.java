c18369487508f9347e47d0b6287bbc6288d54dec
Lalit Singh <lalitthesingh@gmail.com>
2017-07-09 23:33:01 +0530

    Logo added

--- app/src/main/java/com/example/admin/successcoachingapp/SplashScreen.java
+++ app/src/main/java/com/example/admin/successcoachingapp/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_newsplash);
         Thread th= new Thread(){
             @Override
             public void run() {
